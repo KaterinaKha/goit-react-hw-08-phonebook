1be7965a69de5137e00d224fe5ab4bca935d322a
KaterinaKha <katerinakhaa@gmail.com>
2023-08-15 16:59:48 +0200

    reducers, selectors

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,14 @@
 import { Login } from 'components/Login/Login';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { selectAuthentificated } from 'redux/selectors';
 
 const LoginPage = () => {
+  const authentificated = useSelector(selectAuthentificated);
+
+  if (authentificated) return <Navigate to="/contacts" />;
+
   return (
     <>
       <Login />
